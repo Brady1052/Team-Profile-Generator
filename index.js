c4d902c2869b3ea86777dbbb277d27245de675ef
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-15 20:01:44 -0500

    the manager writeFile function works. And the appendFile function for the engineer work

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function firstPrompt() {
             {
                 type: 'input',
                 message: "Please enter the managers employee ID",
-                name: 'manageremail',
+                name: 'managerID',
             },
             {
                 type: 'input',
@@ -27,6 +27,13 @@ function firstPrompt() {
             },
         ])
         .then((answers) => {
+            fs.writeFile('index.html', generateHTML(answers), 'UTF-8',(err)=>{
+                if(err){
+                    console.log(err)
+                }else {
+                    console.log('Success!')
+                }
+            })
             console.log(answers)
             secondPrompt()
         })
@@ -80,12 +87,19 @@ name:'username'
 }
 
     ])
- .then((answers) => {
-console.log(answers)
-secondPrompt()
- })
+    .then((answers) => {
+        fs.appendFile('index.html', generateEngineer(answers), 'UTF-8', (err) =>{
+            if (err){
+                console.log(err)
+            } else {
+                console.log('Success!')
+            }
+            secondPrompt()
+        })
+    })
 }
 
+ 
 function addIntern(){
     console.log('you added an Intern!')
     inquirer.prompt([
@@ -115,3 +129,44 @@ function addIntern(){
         secondPrompt()
          })  
 }
+
+function generateHTML({managerName,managerID,managerEmailAddress,managerOfficeNumber}){
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+    <div class="card" style="width: 18rem;">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4SZai1T5EDDaee4FnstiiwB23CpsRSODqag&usqp=CAU" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Manager</h5>
+      <h3 class="card-text">${managerName}</h3>
+      <p class="card-text">ID:${managerID}</p>
+      <p class="card-text">Email:${managerEmailAddress}</p>
+      <p class="card-text">Office Number:${managerOfficeNumber}</p>
+    </div>
+    </div> 
+    `
+    }
+
+    function generateEngineer ({name, ID, email, username}) {
+     return `
+     <div class="card" style="width: 18rem;">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4SZai1T5EDDaee4FnstiiwB23CpsRSODqag&usqp=CAU" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Manager</h5>
+      <h3 class="card-text">${name}</h3>
+      <p class="card-text">ID:${ID}</p>
+      <p class="card-text">Email:${email}</p>
+      <p class="card-text">Office Number:${username}</p>
+    </div>
+    </div>  
+     `   
+    }
